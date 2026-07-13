581cf27b5bb7aac2d557ea8876e63159f126e05a
firdavs <sharifasanakulova49@gmail.com>
2026-07-13 11:01:20 +0500

    nation

--- blockapp/views.py
+++ blockapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Post
+from django.views.generic import ListView
 
-# Create your views here.
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 5
+    template_name = 'list.html'
+
+def post_detail(request,year,month,day,slug):
+    post = get_object_or_404(Post,slug=slug,
+                             status="publish",
+                             publish_year=year,
+                             publish_month=month,
+                             publish_day=day,)
+    return render(request,"detail.html",{'post':post})
